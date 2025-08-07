910d3598497959b1d6237f9695401d546a13d468
稚名不带撇 <3566983494@qq.com>
2025-08-08 02:48:43 +0800

    Java脚手架项目

---
+++ zmbdp-common/zmbdp-common-core/src/main/java/com/zmbdp/common/core/utils/AESUtil.java
@@ -0,0 +1,41 @@
+package com.zmbdp.common.core.utils;
+
+import cn.hutool.crypto.SecureUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * AES加密工具类
+ */
+public class AESUtil {
+
+    /**
+     * 前后端规定的秘钥
+     */
+    private static final byte[] KEYS = "12345678abcdefgh".getBytes(StandardCharsets.UTF_8);
+
+    /**
+     * aes加密
+     * @param data 原始数据
+     * @return 加密后的数据
+     */
+    public static String encryptHex(String data) {
+        if (StringUtils.isNotEmpty(data)) {
+            return SecureUtil.aes(KEYS).encryptHex(data);
+        }
+        return null;
+    }
+
+    /**
+     * aes解密
+     * @param data 加密后的数据
+     * @return 原始数据
+     */
+    public static String decryptHex(String data) {
+        if (StringUtils.isNotEmpty(data)) {
+            return SecureUtil.aes(KEYS).decryptStr(data);
+        }
+        return null;
+    }
+}
