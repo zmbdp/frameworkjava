d12db6bed6890b3b215a8cdea59e05a0a5966a98
稚名不带撇 <3566983494@qq.com>
2025-08-11 19:20:17 +0800

    Java脚手架项目

--- zmbdp-admin/zmbdp-admin-api/src/main/java/com/zmbdp/admin/api/config/frign/DictionaryFeignClient.java
+++
@@ -1,8 +0,0 @@
-package com.zmbdp.admin.api.config.frign;
-
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "zmbdp-admin-service", path = "/dictionary_type")
-public interface DictionaryFeignClient {
-}
--- zmbdp-common/zmbdp-common-core/src/main/java/com/zmbdp/common/core/constants/CommonConstants.java
+++
@@ -1,5 +0,0 @@
-package com.zmbdp.common.core.constants;
-
-public class CommonConstants {
-    public static final String STANDARD_FORMAT = "yyyy-MM-dd HH:mm:ss";
-}
